51b04068c161cdebbbaf30954e9de54cf56ad158
adihreceniuc <adihreceniuc@yahoo.com>
2016-06-10 23:53:40 -0700

    Add files via upload

---
+++ bricks/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.minge = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.bara = New System.Windows.Forms.Button()
+        Me.scr = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StartToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RestartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InfoToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AutorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.IesireToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'minge
+        '
+        Me.minge.BackColor = System.Drawing.Color.Lime
+        Me.minge.Enabled = False
+        Me.minge.Location = New System.Drawing.Point(324, 331)
+        Me.minge.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.minge.Name = "minge"
+        Me.minge.Size = New System.Drawing.Size(32, 30)
+        Me.minge.TabIndex = 0
+        Me.minge.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 5
+        '
+        'bara
+        '
+        Me.bara.Location = New System.Drawing.Point(237, 566)
+        Me.bara.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.bara.Name = "bara"
+        Me.bara.Size = New System.Drawing.Size(175, 28)
+        Me.bara.TabIndex = 1
+        Me.bara.UseVisualStyleBackColor = True
+        '
+        'scr
+        '
+        Me.scr.AutoSize = True
+        Me.scr.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.scr.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.scr.Location = New System.Drawing.Point(299, 30)
+        Me.scr.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.scr.Name = "scr"
+        Me.scr.Size = New System.Drawing.Size(81, 32)
+        Me.scr.TabIndex = 2
+        Me.scr.Text = "Scor:"
+        Me.scr.Visible = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.InfoToolStripMenuItem1, Me.AutorToolStripMenuItem, Me.IesireToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(8, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(691, 28)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'StartToolStripMenuItem
+        '
+        Me.StartToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem1, Me.RestartToolStripMenuItem})
+        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
+        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(52, 24)
+        Me.StartToolStripMenuItem.Text = "Start"
+        '
+        'StartToolStripMenuItem1
+        '
+        Me.StartToolStripMenuItem1.Name = "StartToolStripMenuItem1"
+        Me.StartToolStripMenuItem1.Size = New System.Drawing.Size(152, 24)
+        Me.StartToolStripMenuItem1.Text = "Start"
+        '
+        'RestartToolStripMenuItem
+        '
+        Me.RestartToolStripMenuItem.Enabled = False
+        Me.RestartToolStripMenuItem.Name = "RestartToolStripMenuItem"
+        Me.RestartToolStripMenuItem.Size = New System.Drawing.Size(152, 24)
+        Me.RestartToolStripMenuItem.Text = "Restart"
+        '
+        'InfoToolStripMenuItem1
+        '
+        Me.InfoToolStripMenuItem1.Name = "InfoToolStripMenuItem1"
+        Me.InfoToolStripMenuItem1.Size = New System.Drawing.Size(47, 24)
+        Me.InfoToolStripMenuItem1.Text = "Info"
+        '
+        'AutorToolStripMenuItem
+        '
+        Me.AutorToolStripMenuItem.Name = "AutorToolStripMenuItem"
+        Me.AutorToolStripMenuItem.Size = New System.Drawing.Size(58, 24)
+        Me.AutorToolStripMenuItem.Text = "Autor"
+        '
+        'IesireToolStripMenuItem
+        '
+        Me.IesireToolStripMenuItem.Name = "IesireToolStripMenuItem"
+        Me.IesireToolStripMenuItem.Size = New System.Drawing.Size(56, 24)
+        Me.IesireToolStripMenuItem.Text = "Iesire"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlText
+        Me.BackgroundImage = Global.bricks.My.Resources.Resources.brick
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(691, 516)
+        Me.Controls.Add(Me.scr)
+        Me.Controls.Add(Me.bara)
+        Me.Controls.Add(Me.minge)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "bricks"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents minge As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents bara As System.Windows.Forms.Button
+    Friend WithEvents scr As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents StartToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InfoToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AutorToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents IesireToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents StartToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RestartToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ bricks/Form1.vb
@@ -0,0 +1,182 @@
+﻿Public Class Form1
+    Dim Vspeed As Single = 4
+    Dim Ospeed As Single = -4
+    Dim linii As Integer = 9
+    Dim coloane As Integer = 8
+    Dim pozitiecaramida As Single = 0.15
+    Dim inaltimecaramida As Single = 0.05
+    Dim scor As Single = 0
+    Dim check As Boolean = False
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        'Verificare partea de sus a ecranului
+
+        If minge.Top < 0 Then
+            Vspeed = -Vspeed
+            My.Computer.Audio.Play(My.Resources.misc124, AudioPlayMode.Background)
+        End If
+        'Verificare stanga ecranului
+
+        If minge.Left < 0 Then
+            Ospeed = -Ospeed
+            My.Computer.Audio.Play(My.Resources.misc124, AudioPlayMode.Background)
+        End If
+        'Verificare partea de jos a ecranului
+        If minge.Bottom > Me.ClientRectangle.Height Then
+            minge.Visible = False
+            Timer1.Enabled = False
+            My.Computer.Audio.Play(My.Resources.gameover, AudioPlayMode.Background)
+            MsgBox("Ai pierdut!!!")
+        End If
+        'Verificare partea dreapta a ecranului
+        If minge.Right > Me.ClientRectangle.Width Then
+            Ospeed = -Ospeed
+            My.Computer.Audio.Play(My.Resources.misc124, AudioPlayMode.Background)
+        End If
+
+        Dim c As Single = minge.Left + minge.Width / 2
+        'Verificare lovitura bara
+        If c > bara.Left And c < bara.Right And Vspeed > 0 And minge.Bottom > bara.Top And minge.Top < bara.Top Then
+            Vspeed = -Vspeed
+            My.Computer.Audio.Play(My.Resources.misc124, AudioPlayMode.Background)
+        End If
+        'Verificare lovitura caramida
+        Dim cnt As Control
+        For Each cnt In Me.Controls
+            If cnt.Name = "caramida" Then
+                Checkcaramida(cnt, minge)
+            End If
+        Next
+        minge.Left += Ospeed
+        minge.Top += Vspeed
+
+    End Sub
+    'functia de verificare lovitura caramida
+    Private Sub Checkcaramida(ByVal caramida As Button, ByVal minge As Button)
+        If caramida.Visible Then
+            Dim lovit As Boolean = False
+            Dim c As Single = minge.Left + minge.Width / 2
+            'Verificare partea de jos a caramizii
+            If Vspeed < 0 And c > caramida.Left And c < caramida.Right And minge.Top < caramida.Bottom And minge.Bottom > caramida.Bottom Then
+                Vspeed = -Vspeed
+                lovit = True
+            End If
+            'Verificare partea de sus a caramizii
+            If Vspeed > 0 And c > caramida.Left And c < caramida.Right And minge.Bottom > caramida.Top And minge.Top < caramida.Top Then
+                Vspeed = -Vspeed
+                lovit = True
+            End If
+            c = minge.Top + minge.Height / 2
+            'Verificarea partea stanga a caramizii
+            If Ospeed > 0 And c > caramida.Top And c < caramida.Bottom And minge.Right > caramida.Left And minge.Left < caramida.Left Then
+                Ospeed = -Ospeed
+                lovit = True
+            End If
+            'Verificarea partea dreapta a caramizii
+            If Ospeed < 0 And c > caramida.Top And c < caramida.Bottom And minge.Left < caramida.Right And minge.Right > caramida.Right Then
+                Ospeed = -Ospeed
+                lovit = True
+            End If
+            If lovit Then If caramida.BackColor = Color.DarkBlue Then caramida.BackColor = Color.Purple Else If caramida.BackColor = Color.Purple Then caramida.BackColor = Color.Red Else If caramida.BackColor = Color.Red Then caramida.Visible = False
+            If lovit Then My.Computer.Audio.Play(My.Resources.hit, AudioPlayMode.Background)
+            If caramida.Visible = False Then scor = scor + 1
+            scr.Text = scor
+            scr.Visible = True
+        End If
+
+    End Sub
+    'Functia de creare caramizi
+    Private Sub caramizi()
+        For Each item As Control In Me.Controls
+            If TypeOf item Is Button Then
+                item.Hide()
+            End If
+        Next
+
+        For i As Integer = 0 To linii - 1
+            For z As Integer = 0 To coloane - 1
+                Dim b As New Button
+                Me.Controls.Add(b)
+                b.Visible = True
+                b.Name = "caramida"
+                b.Width = Me.ClientRectangle.Width / coloane
+                b.Height = Me.ClientRectangle.Height * inaltimecaramida
+                b.Left = z * b.Width
+                b.Top = Me.ClientRectangle.Height * (pozitiecaramida + i * inaltimecaramida)
+                b.BackColor = Color.Purple
+                b.Enabled = False
+                If (i > linii - 4) Then
+                    b.BackColor = Color.DarkBlue
+                End If
+                If (i < linii - 6) Then
+                    b.BackColor = Color.Red
+                End If
+            Next
+        Next
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Enabled = False
+        minge.Visible = False
+        bara.Visible = False
+
+    End Sub
+    'bara de jos
+    Private Sub Form1_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseMove
+        bara.Width = 0.3 * Me.ClientRectangle.Width
+        bara.Height = 0.05 * Me.ClientRectangle.Height
+        bara.Top = 0.95 * Me.ClientRectangle.Height
+        bara.Left = e.X - (0.15 * Me.ClientRectangle.Width)
+        If scor = linii * coloane And check = False Then
+            minge.Hide()
+            minge.Enabled = False
+            Timer1.Enabled = False
+            check = True
+            My.Computer.Audio.Play(My.Resources.applause3, AudioPlayMode.Background)
+            MsgBox("Felicitari!!!")
+            bara.Hide()
+        End If
+    End Sub
+
+    Private Sub IesireToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles IesireToolStripMenuItem.Click
+        If MsgBox("Sigur doriti sa inchideti programul", vbOKCancel, "iesire") = MsgBoxResult.Cancel Then
+            Exit Sub
+        Else
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub AutorToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AutorToolStripMenuItem.Click
+        MsgBox("Hreceniuc Adi,grupa 3122A,Proiect PAG,Bricks")
+    End Sub
+
+    Private Sub StartToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles StartToolStripMenuItem1.Click
+
+        BackgroundImage = Nothing
+        RestartToolStripMenuItem.Enabled = True
+        'incarcare caramizi
+        caramizi()
+        Timer1.Enabled = True
+        minge.Visible = True
+        bara.Visible = True
+        StartToolStripMenuItem1.Enabled = False
+    End Sub
+
+    Private Sub RestartToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RestartToolStripMenuItem.Click
+        scor = 0
+        caramizi()
+        Timer1.Enabled = True
+        minge.Visible = True
+        bara.Visible = True
+        minge.Location = New Point(243, 243)
+
+    End Sub
+
+    Private Sub InfoToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles InfoToolStripMenuItem1.Click
+        MsgBox("Se joaca cu ajutorul mouseului.Distruge toate caramizile!!!")
+    End Sub
+
+    Private Sub StartToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles StartToolStripMenuItem.Click
+    End Sub
+End Class
